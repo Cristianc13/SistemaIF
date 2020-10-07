434d928cef419f15da8488684c11ebfd6842bb4a
bycanalesvalles@gmail.com <bycanalesvalles@gmail.com>
2020-10-07 16:01:03 -0600

    Creacion de Conexion y CADCliente

---
+++ SistemaFacturacion/CAD/CADConexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CAD
+{
+    internal class CADConexion
+    {
+        private SqlConnection Conexion = new SqlConnection(
+  "Data Source=localhost;Initial Catalog=TiendaInventario;Integrated Security=True");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+            {
+                Conexion.Open();
+            }
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+            {
+                Conexion.Close();
+            }
+            return Conexion;
+        }
+    }
+}
