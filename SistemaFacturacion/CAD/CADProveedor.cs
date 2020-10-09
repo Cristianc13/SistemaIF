cb41ebf950753333f57e1e72ce2834daa0e35761
bycanalesvalles@gmail.com <bycanalesvalles@gmail.com>
2020-10-08 19:30:20 -0600

    Cambios Proveedor

---
+++ SistemaFacturacion/CAD/CADProveedor.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using ENT;
+
+namespace CAD
+{
+    public class CADProveedor : CADConexion
+    {
+        private DataTable tabla = new DataTable();
+
+        public DataTable MostrarProveedor()
+        {
+            tabla.Clear();
+            SqlCommand cmd = new SqlCommand("SelectProveedor", AbrirConexion());
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(tabla);
+            CerrarConexion();
+            return tabla;
+        }
+
+        public void InsertProveedor(ENTProveedor EProveedor)
+        {
+            SqlCommand cmd = new SqlCommand("InsertProveedor", AbrirConexion());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@nombreProv", EProveedor.nombreProv);
+            cmd.Parameters.AddWithValue("@telefonoProv", EProveedor.telefonoProv);
+            cmd.Parameters.AddWithValue("@nombreCompañia", EProveedor.nombreCompañia);
+            cmd.Parameters.AddWithValue("@numeroCompañia", EProveedor.numeroCompañia);
+            cmd.ExecuteNonQuery();
+            CerrarConexion();
+        }
+    }
+}
---
+++ SistemaFacturacion/ENT/ENTProveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class ENTProveedor
+    {
+        public int idProveedor { get; set; }
+        public string nombreProv { get; set; }
+        public string telefonoProv { get; set; }
+        public string nombreCompañia { get; set; }
+        public string numeroCompañia { get; set; }
+    }
+}
