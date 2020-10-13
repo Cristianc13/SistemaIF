0eca594ad897e52b2a81cd48856bfa9ba63ca62a
AndruMaster69 <AndruMaster69@DESKTOP-8AT2UCU>
2020-10-12 20:15:46 -0600

    WINMENU

---
+++ SistemaFacturacion/WIN/WINMENU.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WIN
+{
+    public partial class WINMENU : Form
+    {
+        public WINMENU()
+        {
+            InitializeComponent();
+            personalizarDiseño();
+        }
+        private void personalizarDiseño()
+        {
+            PanelSubMenuCatalogos.Visible = false;
+            PanelSubMenuMaestro.Visible = false;
+            PanelSubMenuProductos.Visible = false;
+        }
+        private void ocultarSubMenu()
+        {
+            if (PanelSubMenuCatalogos.Visible == true)
+                PanelSubMenuCatalogos.Visible = false;
+            if (PanelSubMenuMaestro.Visible == true)
+                PanelSubMenuMaestro.Visible = false;
+            if (PanelSubMenuProductos.Visible == true)
+                PanelSubMenuProductos.Visible = false;
+        }
+        private void mostrarSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                ocultarSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void btnCatalogo_Click(object sender, EventArgs e)
+        {
+            mostrarSubMenu(PanelSubMenuCatalogos);
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+            //codigo aqui
+            ocultarSubMenu();
+        }
+
+        private void btnCategorias_Click(object sender, EventArgs e)
+        {
+            //codigo aqui
+            ocultarSubMenu();
+        }
+
+        private void btnCliente_Click(object sender, EventArgs e)
+        {
+            openChildForm(new WINCliente());
+            //codigo aqui
+            ocultarSubMenu();
+        }
+
+        private void btnEstado_Click(object sender, EventArgs e)
+        {
+            //codigo aqui
+            ocultarSubMenu();
+        }
+
+        private void btnMarca_Click(object sender, EventArgs e)
+        {
+            openChildForm(new WINMarca());
+            //codigo aqui
+            ocultarSubMenu();
+        }
+
+        private void btnModelo_Click(object sender, EventArgs e)
+        {
+            //codigo aqui
+            ocultarSubMenu();
+        }
+
+        private void btnProveedor_Click(object sender, EventArgs e)
+        {
+            //codigo aqui
+            ocultarSubMenu();
+        }
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm!=null)
+            
+                activeForm.Close();
+                activeForm = childForm;
+                childForm.TopLevel = false;
+                childForm.FormBorderStyle = FormBorderStyle.None;
+                childForm.Dock = DockStyle.Fill;
+                Panelchild.Controls.Add(childForm);
+                Panelchild.Tag = childForm;
+                childForm.BringToFront();
+                childForm.Show();
+
+            
+        }
+    }
+}
