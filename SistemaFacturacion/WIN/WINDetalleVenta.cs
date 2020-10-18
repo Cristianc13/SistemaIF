e77fb414d8f0c7a302a62618bb422f3142c0b3bd
Alberto José Blandón Moreno <ialbertbla98@gmail.com>
2020-10-17 18:39:16 -0600

    WINDetalleVenta

--- SistemaFacturacion/WIN/WINDetalleVenta.cs
+++ SistemaFacturacion/WIN/WINDetalleVenta.cs
@@ -25,6 +25,8 @@ namespace WIN
 
         private void WINDetalleVenta_Load(object sender, EventArgs e)
         {
+            ImportetextBox.Text = "0";
+            HabilitarBotones(false, true);
             LlenaComboProducto();
             Limpiar();
         }
@@ -39,22 +41,40 @@ namespace WIN
 
         private void Limpiar()
         {
-            ProductocomboBox.SelectedIndex = -1;
+            ProductocomboBox.Text = string.Empty; 
             CantidadtextBox.Text = string.Empty;
             PreciotextBox.Text = string.Empty;
-            ProductocomboBox.Focus();
+            errorProvider1.Clear();
+            ProductocomboBox.SelectedIndex = -1;
+            ProductocomboBox.Focus();           
         }
 
         private void Agregarbutton_Click(object sender, EventArgs e)
         {
+            if (ProductocomboBox.SelectedIndex == -1)
+            {
+                errorProvider1.SetError(ProductocomboBox, "Debe ingresar un Producto");
+                return;
+            }
+            errorProvider1.Clear();
+
+            if (CantidadtextBox.Text == string.Empty)
+            {
+                errorProvider1.SetError(CantidadtextBox, "Debe ingresar una Cantidad");
+                return;
+            }
+            errorProvider1.Clear();
+
             string Nombre = ProductocomboBox.Text;// ProductocomboBox.SelectedItem;
             string Cantidad = CantidadtextBox.Text;
             string Precio = PreciotextBox.Text;
-            string Importe = (int.Parse(Cantidad) * int.Parse(Precio)).ToString();
+            string Importe = (decimal.Parse(Cantidad) * decimal.Parse(Precio)).ToString();
             string idP = idProducto.ToString();
             DVentadataGridView.Rows.Add(new object[] { Nombre, Cantidad, Precio, 15, Importe, "Eliminar", idP });
             CalcularTotal();
             Limpiar();
+            HabilitarGuardar();
+            errorProvider1.Clear();
         }
 
         private void ProductocomboBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -95,6 +115,8 @@ namespace WIN
 
         private void Guardarbutton_Click(object sender, EventArgs e)
         {
+            
+
             try
             {
                 List<ENTDetalleVenta> lst = new List<ENTDetalleVenta>();
@@ -109,6 +131,10 @@ namespace WIN
                     oConcepto.importe = decimal.Parse(dr.Cells[4].Value.ToString());
                     oConcepto.Fk_idProducto = int.Parse(dr.Cells[6].Value.ToString());
                     lst.Add(oConcepto);
+                    DVentadataGridView.Rows.Clear();
+                    ImportetextBox.Text = "0";
+                    ProductocomboBox.Focus();
+                    HabilitarBotones(false, true);
                 }
 
                 BLDetalle.InsertDetalleVenta(2, lst);
@@ -121,8 +147,52 @@ namespace WIN
             }
         }
 
+        private void HabilitarBotones(bool p1, bool p2)
+        {
+            Guardarbutton.Enabled = p1;            
+           //Cancelarbutton.Enabled = p1;
+        }
+
+
+        public void HabilitarGuardar()
+        {
+            if (DVentadataGridView.Rows.Count == 0)
+            {
+                HabilitarBotones(false, true);
+            }
+            else
+            {
+                HabilitarBotones(true, false);
+            }
+        }
+
         private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
         {
+        }
+
+        private void Cancelarbutton_Click(object sender, EventArgs e)
+        {
+
+            Limpiar();
+        }
+
+        private void CantidadtextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CantidadtextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // solo 1 punto decimal
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
