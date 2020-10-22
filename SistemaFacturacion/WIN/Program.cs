2a84a9c45895f15baf323a6bbaac9e534bfc0d12
AndruMaster69 <AndruMaster69@DESKTOP-8AT2UCU>
2020-10-21 21:16:59 -0600

    //

--- SistemaFacturacion/WIN/Program.cs
+++ SistemaFacturacion/WIN/Program.cs
@@ -9,6 +9,7 @@ namespace WIN
     internal static class Program
     {
         /// <summary>
+        /// //
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
