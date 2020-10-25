30c2cc5c41e0b499dbff465bcb170e7172040da7
bycanalesvalles@gmail.com <bycanalesvalles@gmail.com>
2020-10-24 19:33:10 -0600

    Exportar Excel

--- SistemaFacturacion/WIN/WINProducto.cs
+++ SistemaFacturacion/WIN/WINProducto.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 using BL;
+using Syncfusion.XlsIO;
 using ENT;
 
 namespace WIN
@@ -337,6 +340,11 @@ namespace WIN
             LlenarMarca();
         }
 
+        private void Exportarbutton_Click(object sender, EventArgs e)
+        {
+            ExportarDatos(ProductodataGridView);
+        }
+
         public void LlenarCategoria()
         {
             CategoriacomboBox.DataSource = BCategoria.MostrarCategoria();
@@ -442,6 +450,37 @@ namespace WIN
         public void CantidadProducto()
         {
             CantidadProduLabel.Text = BProducto.CantidadProducto().ToString();
+        }
+
+        public void ExportarDatos(DataGridView datalistado)
+        {
+            try
+            {
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application(); // Instancia a la libreria de Microsoft Office
+                excel.Application.Workbooks.Add(true); //Con esto añadimos una hoja en el Excel para exportar los archivos
+                int IndiceColumna = 0;
+                foreach (DataGridViewColumn columna in datalistado.Columns) //Aquí empezamos a leer las columnas del listado a exportar
+                {
+                    IndiceColumna++;
+                    excel.Cells[1, IndiceColumna] = columna.Name;
+                }
+                int IndiceFila = 0;
+                foreach (DataGridViewRow fila in datalistado.Rows) //Aquí leemos las filas de las columnas leídas
+                {
+                    IndiceFila++;
+                    IndiceColumna = 0;
+                    foreach (DataGridViewColumn columna in datalistado.Columns)
+                    {
+                        IndiceColumna++;
+                        excel.Cells[IndiceFila + 1, IndiceColumna] = fila.Cells[columna.Name].Value;
+                    }
+                }
+                excel.Visible = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No hay Registros a Exportar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
