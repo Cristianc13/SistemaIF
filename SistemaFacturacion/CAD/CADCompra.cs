6cf1f5af6b0e7ffadfabec380153745402d57b63
AndruMaster69 <AndruMaster69@DESKTOP-8AT2UCU>
2020-10-24 19:31:15 -0600

    1. Creacion de WINCompra 2. Creacion de CADCompra 3. Creacion de BLCompra

---
+++ SistemaFacturacion/CAD/CADCompra.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENT;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CAD
+{
+    public class CADCompra : CADConexion
+    {
+        private DataTable tabla = new DataTable();
+
+        public DataTable MostrarCompra()
+        {
+            tabla.Clear();
+            SqlCommand cmd = new SqlCommand("SelectCompra", AbrirConexion());
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(tabla);
+            CerrarConexion();
+            return tabla;
+        }
+
+        public void InsertCompra(ENTCompra Ecompra)
+        {
+            SqlCommand cmd = new SqlCommand("InsertCompra", AbrirConexion());
+            cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Parameters.AddWithValue("@idCompra", Ecompra.idCompra);
+            cmd.Parameters.AddWithValue("@numeroFactura", Ecompra.numeroFactura);
+            //cmd.Parameters.AddWithValue("@fechaCompra", Ecompra.fechaCompra);
+            cmd.Parameters.AddWithValue("@descripcion", Ecompra.descripcion);
+            cmd.Parameters.AddWithValue("@FK_idProveedor", Ecompra.FK_idProveedor);
+            cmd.ExecuteNonQuery();
+            CerrarConexion();
+        }
+
+        public void DeleteCompra(ENTCompra Ecompra)
+        {
+            SqlCommand cmd = new SqlCommand("DeleteCompra", AbrirConexion());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@idCompra", Ecompra.idCompra);
+            cmd.ExecuteNonQuery();
+            CerrarConexion();
+        }
+
+        public void UpdateCompra(ENTCompra Ecompra)
+        {
+            SqlCommand cmd = new SqlCommand("UpdateCompra", AbrirConexion());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@idCompra", Ecompra.idCompra);
+            cmd.Parameters.AddWithValue("@numeroFactura", Ecompra.numeroFactura);
+            cmd.Parameters.AddWithValue("@descripcion", Ecompra.descripcion);
+            cmd.Parameters.AddWithValue("@FK_idProveedor", Ecompra.FK_idProveedor);
+            cmd.ExecuteNonQuery();
+            CerrarConexion();
+        }
+
+
+
+    }
+}
---
+++ SistemaFacturacion/WIN/WINCompra.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WIN
+{
+    partial class WINCompra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cliente = new System.Windows.Forms.Label();
+            this.txtNFactura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtdescrip = new System.Windows.Forms.TextBox();
+            this.ProveedorcomboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Cancelarbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.Actualizarbutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.CompraGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.CompraGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Cliente
+            // 
+            this.Cliente.AutoSize = true;
+            this.Cliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cliente.Location = new System.Drawing.Point(12, 25);
+            this.Cliente.Name = "Cliente";
+            this.Cliente.Size = new System.Drawing.Size(84, 20);
+            this.Cliente.TabIndex = 14;
+            this.Cliente.Text = "N° Factura";
+            // 
+            // txtNFactura
+            // 
+            this.txtNFactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNFactura.Location = new System.Drawing.Point(107, 24);
+            this.txtNFactura.Name = "txtNFactura";
+            this.txtNFactura.Size = new System.Drawing.Size(231, 23);
+            this.txtNFactura.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Descripcion";
+            // 
+            // txtdescrip
+            // 
+            this.txtdescrip.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdescrip.Location = new System.Drawing.Point(107, 65);
+            this.txtdescrip.Name = "txtdescrip";
+            this.txtdescrip.Size = new System.Drawing.Size(231, 23);
+            this.txtdescrip.TabIndex = 17;
+            // 
+            // ProveedorcomboBox
+            // 
+            this.ProveedorcomboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.ProveedorcomboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.ProveedorcomboBox.DropDownHeight = 115;
+            this.ProveedorcomboBox.DropDownWidth = 240;
+            this.ProveedorcomboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProveedorcomboBox.FormattingEnabled = true;
+            this.ProveedorcomboBox.IntegralHeight = false;
+            this.ProveedorcomboBox.Location = new System.Drawing.Point(107, 112);
+            this.ProveedorcomboBox.Name = "ProveedorcomboBox";
+            this.ProveedorcomboBox.Size = new System.Drawing.Size(231, 24);
+            this.ProveedorcomboBox.TabIndex = 18;
+            this.ProveedorcomboBox.SelectedIndexChanged += new System.EventHandler(this.ProveedorcomboBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Proveedor";
+            // 
+            // Cancelarbutton
+            // 
+            this.Cancelarbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Cancelarbutton.Location = new System.Drawing.Point(305, 159);
+            this.Cancelarbutton.Name = "Cancelarbutton";
+            this.Cancelarbutton.Size = new System.Drawing.Size(85, 25);
+            this.Cancelarbutton.TabIndex = 23;
+            this.Cancelarbutton.Text = "Cancelar";
+            this.Cancelarbutton.UseVisualStyleBackColor = true;
+            this.Cancelarbutton.Click += new System.EventHandler(this.Cancelarbutton_Click);
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Eliminarbutton.Location = new System.Drawing.Point(208, 159);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(85, 25);
+            this.Eliminarbutton.TabIndex = 22;
+            this.Eliminarbutton.Text = "Eliminar";
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            this.Eliminarbutton.Click += new System.EventHandler(this.Eliminarbutton_Click);
+            // 
+            // Actualizarbutton
+            // 
+            this.Actualizarbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Actualizarbutton.Location = new System.Drawing.Point(114, 159);
+            this.Actualizarbutton.Name = "Actualizarbutton";
+            this.Actualizarbutton.Size = new System.Drawing.Size(85, 25);
+            this.Actualizarbutton.TabIndex = 21;
+            this.Actualizarbutton.Text = "Actualizar";
+            this.Actualizarbutton.UseVisualStyleBackColor = true;
+            this.Actualizarbutton.Click += new System.EventHandler(this.Actualizarbutton_Click);
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Guardarbutton.Location = new System.Drawing.Point(16, 159);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(85, 25);
+            this.Guardarbutton.TabIndex = 20;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // CompraGridView1
+            // 
+            this.CompraGridView1.AllowUserToAddRows = false;
+            this.CompraGridView1.AllowUserToDeleteRows = false;
+            this.CompraGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CompraGridView1.Location = new System.Drawing.Point(15, 212);
+            this.CompraGridView1.Name = "CompraGridView1";
+            this.CompraGridView1.ReadOnly = true;
+            this.CompraGridView1.Size = new System.Drawing.Size(773, 226);
+            this.CompraGridView1.TabIndex = 24;
+            this.CompraGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CompraGridView1_CellClick);
+            // 
+            // WINCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CompraGridView1);
+            this.Controls.Add(this.Cancelarbutton);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Actualizarbutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.ProveedorcomboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtdescrip);
+            this.Controls.Add(this.Cliente);
+            this.Controls.Add(this.txtNFactura);
+            this.Name = "WINCompra";
+            this.Text = "WINCompra";
+            this.Load += new System.EventHandler(this.WINCompra_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CompraGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Cliente;
+        public System.Windows.Forms.TextBox txtNFactura;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtdescrip;
+        public System.Windows.Forms.ComboBox ProveedorcomboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Cancelarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.Button Actualizarbutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.DataGridView CompraGridView1;
+    }
+}
