b086c75c95c874cff5fd39a421ea4bd8d25fc945
bycanalesvalles@gmail.com <bycanalesvalles@gmail.com>
2020-12-03 20:09:38 -0600

    Cambios Producto

--- SistemaFacturacion/WIN/WINCategoria.Designer.cs
+++ SistemaFacturacion/WIN/WINCategoria.Designer.cs
@@ -41,15 +41,26 @@
             this.CategoriadataGridView = new System.Windows.Forms.DataGridView();
             this.label3 = new System.Windows.Forms.Label();
             this.CodigotextBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btncancelar = new FontAwesome.Sharp.IconButton();
+            this.btneliminar = new FontAwesome.Sharp.IconButton();
+            this.btneditar = new FontAwesome.Sharp.IconButton();
+            this.btnguardar = new FontAwesome.Sharp.IconButton();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.CategoriadataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Location = new System.Drawing.Point(52, 19);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 20);
@@ -60,7 +71,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(10, 104);
+            this.label2.Location = new System.Drawing.Point(38, 95);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 20);
@@ -69,7 +80,7 @@
             // 
             // CategoriatextBox
             // 
-            this.CategoriatextBox.Location = new System.Drawing.Point(111, 28);
+            this.CategoriatextBox.Location = new System.Drawing.Point(139, 19);
             this.CategoriatextBox.Margin = new System.Windows.Forms.Padding(2);
             this.CategoriatextBox.Name = "CategoriatextBox";
             this.CategoriatextBox.Size = new System.Drawing.Size(214, 20);
@@ -77,7 +88,8 @@
             // 
             // DescripciontextBox
             // 
-            this.DescripciontextBox.Location = new System.Drawing.Point(111, 108);
+            this.DescripciontextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DescripciontextBox.Location = new System.Drawing.Point(139, 99);
             this.DescripciontextBox.Margin = new System.Windows.Forms.Padding(2);
             this.DescripciontextBox.Multiline = true;
             this.DescripciontextBox.Name = "DescripciontextBox";
@@ -87,7 +99,7 @@
             // Guardarbutton
             // 
             this.Guardarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Guardarbutton.Location = new System.Drawing.Point(19, 383);
+            this.Guardarbutton.Location = new System.Drawing.Point(42, 132);
             this.Guardarbutton.Margin = new System.Windows.Forms.Padding(2);
             this.Guardarbutton.Name = "Guardarbutton";
             this.Guardarbutton.Size = new System.Drawing.Size(75, 24);
@@ -99,7 +111,7 @@
             // Actualizarbutton
             // 
             this.Actualizarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Actualizarbutton.Location = new System.Drawing.Point(111, 383);
+            this.Actualizarbutton.Location = new System.Drawing.Point(134, 132);
             this.Actualizarbutton.Margin = new System.Windows.Forms.Padding(2);
             this.Actualizarbutton.Name = "Actualizarbutton";
             this.Actualizarbutton.Size = new System.Drawing.Size(83, 24);
@@ -111,7 +123,7 @@
             // Eliminarbutton
             // 
             this.Eliminarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Eliminarbutton.Location = new System.Drawing.Point(208, 383);
+            this.Eliminarbutton.Location = new System.Drawing.Point(231, 132);
             this.Eliminarbutton.Margin = new System.Windows.Forms.Padding(2);
             this.Eliminarbutton.Name = "Eliminarbutton";
             this.Eliminarbutton.Size = new System.Drawing.Size(75, 24);
@@ -123,7 +135,7 @@
             // Cancelarbutton
             // 
             this.Cancelarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cancelarbutton.Location = new System.Drawing.Point(307, 383);
+            this.Cancelarbutton.Location = new System.Drawing.Point(330, 132);
             this.Cancelarbutton.Margin = new System.Windows.Forms.Padding(2);
             this.Cancelarbutton.Name = "Cancelarbutton";
             this.Cancelarbutton.Size = new System.Drawing.Size(82, 24);
@@ -141,8 +153,10 @@
             this.CategoriadataGridView.AllowUserToAddRows = false;
             this.CategoriadataGridView.AllowUserToDeleteRows = false;
             this.CategoriadataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.CategoriadataGridView.BackgroundColor = System.Drawing.Color.White;
             this.CategoriadataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.CategoriadataGridView.Location = new System.Drawing.Point(19, 201);
+            this.CategoriadataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CategoriadataGridView.Location = new System.Drawing.Point(0, 0);
             this.CategoriadataGridView.Margin = new System.Windows.Forms.Padding(2);
             this.CategoriadataGridView.Name = "CategoriadataGridView";
             this.CategoriadataGridView.ReadOnly = true;
@@ -150,7 +164,7 @@
             this.CategoriadataGridView.RowHeadersWidth = 51;
             this.CategoriadataGridView.RowTemplate.Height = 24;
             this.CategoriadataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.CategoriadataGridView.Size = new System.Drawing.Size(379, 172);
+            this.CategoriadataGridView.Size = new System.Drawing.Size(465, 163);
             this.CategoriadataGridView.TabIndex = 8;
             this.CategoriadataGridView.DoubleClick += new System.EventHandler(this.CategoriadataGridView_DoubleClick);
             // 
@@ -158,7 +172,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(43, 66);
+            this.label3.Location = new System.Drawing.Point(71, 57);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 20);
@@ -167,28 +181,157 @@
             // 
             // CodigotextBox
             // 
-            this.CodigotextBox.Location = new System.Drawing.Point(111, 68);
+            this.CodigotextBox.Location = new System.Drawing.Point(139, 59);
             this.CodigotextBox.Margin = new System.Windows.Forms.Padding(2);
             this.CodigotextBox.Name = "CodigotextBox";
             this.CodigotextBox.Size = new System.Drawing.Size(214, 20);
             this.CodigotextBox.TabIndex = 1;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(30)))), ((int)(((byte)(41)))));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(465, 32);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.btncancelar);
+            this.panel2.Controls.Add(this.btneliminar);
+            this.panel2.Controls.Add(this.btneditar);
+            this.panel2.Controls.Add(this.btnguardar);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.CodigotextBox);
+            this.panel2.Controls.Add(this.Cancelarbutton);
+            this.panel2.Controls.Add(this.CategoriatextBox);
+            this.panel2.Controls.Add(this.Eliminarbutton);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.Actualizarbutton);
+            this.panel2.Controls.Add(this.DescripciontextBox);
+            this.panel2.Controls.Add(this.Guardarbutton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(465, 217);
+            this.panel2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.CategoriadataGridView);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 249);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(465, 163);
+            this.panel3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(9, 6);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Categoria";
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(30)))), ((int)(((byte)(41)))));
+            this.btncancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btncancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btncancelar.ForeColor = System.Drawing.Color.White;
+            this.btncancelar.IconChar = FontAwesome.Sharp.IconChar.Save;
+            this.btncancelar.IconColor = System.Drawing.Color.White;
+            this.btncancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btncancelar.IconSize = 30;
+            this.btncancelar.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btncancelar.Location = new System.Drawing.Point(347, 171);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(109, 35);
+            this.btncancelar.TabIndex = 41;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btncancelar.UseVisualStyleBackColor = false;
+            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(30)))), ((int)(((byte)(41)))));
+            this.btneliminar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btneliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btneliminar.ForeColor = System.Drawing.Color.White;
+            this.btneliminar.IconChar = FontAwesome.Sharp.IconChar.Save;
+            this.btneliminar.IconColor = System.Drawing.Color.White;
+            this.btneliminar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btneliminar.IconSize = 30;
+            this.btneliminar.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btneliminar.Location = new System.Drawing.Point(236, 171);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(104, 35);
+            this.btneliminar.TabIndex = 40;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btneliminar.UseVisualStyleBackColor = false;
+            this.btneliminar.Visible = false;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btneditar
+            // 
+            this.btneditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(30)))), ((int)(((byte)(41)))));
+            this.btneditar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btneditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btneditar.ForeColor = System.Drawing.Color.White;
+            this.btneditar.IconChar = FontAwesome.Sharp.IconChar.Save;
+            this.btneditar.IconColor = System.Drawing.Color.White;
+            this.btneditar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btneditar.IconSize = 30;
+            this.btneditar.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btneditar.Location = new System.Drawing.Point(125, 171);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(104, 35);
+            this.btneditar.TabIndex = 39;
+            this.btneditar.Text = "Editar";
+            this.btneditar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btneditar.UseVisualStyleBackColor = false;
+            this.btneditar.Visible = false;
+            this.btneditar.Click += new System.EventHandler(this.btneditar_Click);
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(30)))), ((int)(((byte)(41)))));
+            this.btnguardar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnguardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnguardar.ForeColor = System.Drawing.Color.White;
+            this.btnguardar.IconChar = FontAwesome.Sharp.IconChar.Save;
+            this.btnguardar.IconColor = System.Drawing.Color.White;
+            this.btnguardar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnguardar.IconSize = 30;
+            this.btnguardar.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnguardar.Location = new System.Drawing.Point(14, 171);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(104, 35);
+            this.btnguardar.TabIndex = 38;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnguardar.UseVisualStyleBackColor = false;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
             // WINCategoria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(419, 428);
-            this.Controls.Add(this.CodigotextBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.CategoriadataGridView);
-            this.Controls.Add(this.Cancelarbutton);
-            this.Controls.Add(this.Eliminarbutton);
-            this.Controls.Add(this.Actualizarbutton);
-            this.Controls.Add(this.Guardarbutton);
-            this.Controls.Add(this.DescripciontextBox);
-            this.Controls.Add(this.CategoriatextBox);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(465, 412);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "WINCategoria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -196,8 +339,12 @@
             this.Load += new System.EventHandler(this.Categoria_Load);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.CategoriadataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -215,5 +362,13 @@
         private System.Windows.Forms.DataGridView CategoriadataGridView;
         private System.Windows.Forms.TextBox CodigotextBox;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private FontAwesome.Sharp.IconButton btncancelar;
+        private FontAwesome.Sharp.IconButton btneliminar;
+        private FontAwesome.Sharp.IconButton btneditar;
+        private FontAwesome.Sharp.IconButton btnguardar;
     }
 }
