e09b6fcd067800ed4993dac57318facf62435bde
Alberto José Blandón Moreno <ialbertbla98@gmail.com>
2021-01-07 16:17:22 -0600

    Cambios y correcciones

--- SistemaFacturacion/WIN/WINReportes/RVenta.Designer.cs
+++ SistemaFacturacion/WIN/WINReportes/RVenta.Designer.cs
@@ -32,6 +32,8 @@
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.DetalleVenta1 = new WIN.WINReportes.ReporteCristal.DetalleVenta();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBuscar = new FontAwesome.Sharp.IconButton();
+            this.textBoxFactura = new System.Windows.Forms.TextBox();
             this.dtfrom = new Bunifu.Framework.UI.BunifuDatepicker();
             this.btnpersonalizado = new FontAwesome.Sharp.IconButton();
             this.aniobtn = new FontAwesome.Sharp.IconButton();
@@ -46,6 +48,9 @@
             this.minimizar = new System.Windows.Forms.PictureBox();
             this.salir = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -53,6 +58,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.btnrestaurar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.minimizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.salir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
@@ -77,18 +83,56 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(30)))), ((int)(((byte)(41)))));
-            this.panel1.Controls.Add(this.dtfrom);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btnpersonalizado);
+            this.panel1.Controls.Add(this.textBoxFactura);
+            this.panel1.Controls.Add(this.dtdate);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.dtfrom);
+            this.panel1.Controls.Add(this.btnBuscar);
             this.panel1.Controls.Add(this.aniobtn);
             this.panel1.Controls.Add(this.totalbtn);
             this.panel1.Controls.Add(this.mesbtn);
             this.panel1.Controls.Add(this.Semanabtn);
-            this.panel1.Controls.Add(this.dtdate);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 44);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(240, 666);
             this.panel1.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.IconChar = FontAwesome.Sharp.IconChar.Search;
+            this.btnBuscar.IconColor = System.Drawing.Color.White;
+            this.btnBuscar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnBuscar.IconSize = 30;
+            this.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBuscar.Location = new System.Drawing.Point(12, 533);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnBuscar.Size = new System.Drawing.Size(205, 46);
+            this.btnBuscar.TabIndex = 52;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBuscar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // textBoxFactura
+            // 
+            this.textBoxFactura.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxFactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFactura.Location = new System.Drawing.Point(23, 501);
+            this.textBoxFactura.MaxLength = 7;
+            this.textBoxFactura.Name = "textBoxFactura";
+            this.textBoxFactura.Size = new System.Drawing.Size(196, 17);
+            this.textBoxFactura.TabIndex = 51;
+            this.textBoxFactura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxFactura_KeyPress);
             // 
             // dtfrom
             // 
@@ -100,9 +144,9 @@
             this.dtfrom.ForeColor = System.Drawing.Color.White;
             this.dtfrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dtfrom.FormatCustom = null;
-            this.dtfrom.Location = new System.Drawing.Point(8, 430);
+            this.dtfrom.Location = new System.Drawing.Point(17, 370);
             this.dtfrom.Name = "dtfrom";
-            this.dtfrom.Size = new System.Drawing.Size(225, 34);
+            this.dtfrom.Size = new System.Drawing.Size(207, 34);
             this.dtfrom.TabIndex = 50;
             this.dtfrom.Value = new System.DateTime(2021, 1, 7, 0, 0, 0, 0);
             // 
@@ -117,10 +161,10 @@
             this.btnpersonalizado.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnpersonalizado.IconSize = 30;
             this.btnpersonalizado.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnpersonalizado.Location = new System.Drawing.Point(6, 481);
+            this.btnpersonalizado.Location = new System.Drawing.Point(12, 410);
             this.btnpersonalizado.Name = "btnpersonalizado";
             this.btnpersonalizado.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
-            this.btnpersonalizado.Size = new System.Drawing.Size(226, 46);
+            this.btnpersonalizado.Size = new System.Drawing.Size(207, 46);
             this.btnpersonalizado.TabIndex = 49;
             this.btnpersonalizado.Text = "Generar";
             this.btnpersonalizado.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -139,7 +183,7 @@
             this.aniobtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.aniobtn.IconSize = 30;
             this.aniobtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.aniobtn.Location = new System.Drawing.Point(8, 274);
+            this.aniobtn.Location = new System.Drawing.Point(6, 183);
             this.aniobtn.Name = "aniobtn";
             this.aniobtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
             this.aniobtn.Size = new System.Drawing.Size(226, 46);
@@ -161,7 +205,7 @@
             this.totalbtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.totalbtn.IconSize = 30;
             this.totalbtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.totalbtn.Location = new System.Drawing.Point(8, 326);
+            this.totalbtn.Location = new System.Drawing.Point(6, 235);
             this.totalbtn.Name = "totalbtn";
             this.totalbtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
             this.totalbtn.Size = new System.Drawing.Size(226, 46);
@@ -183,7 +227,7 @@
             this.mesbtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.mesbtn.IconSize = 30;
             this.mesbtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.mesbtn.Location = new System.Drawing.Point(7, 219);
+            this.mesbtn.Location = new System.Drawing.Point(5, 128);
             this.mesbtn.Name = "mesbtn";
             this.mesbtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
             this.mesbtn.Size = new System.Drawing.Size(226, 46);
@@ -205,7 +249,7 @@
             this.Semanabtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.Semanabtn.IconSize = 30;
             this.Semanabtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Semanabtn.Location = new System.Drawing.Point(8, 164);
+            this.Semanabtn.Location = new System.Drawing.Point(6, 73);
             this.Semanabtn.Name = "Semanabtn";
             this.Semanabtn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
             this.Semanabtn.Size = new System.Drawing.Size(226, 46);
@@ -226,9 +270,9 @@
             this.dtdate.ForeColor = System.Drawing.Color.White;
             this.dtdate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dtdate.FormatCustom = null;
-            this.dtdate.Location = new System.Drawing.Point(8, 384);
+            this.dtdate.Location = new System.Drawing.Point(18, 321);
             this.dtdate.Name = "dtdate";
-            this.dtdate.Size = new System.Drawing.Size(224, 34);
+            this.dtdate.Size = new System.Drawing.Size(206, 34);
             this.dtdate.TabIndex = 44;
             this.dtdate.Value = new System.DateTime(2021, 1, 7, 0, 0, 0, 0);
             // 
@@ -316,6 +360,42 @@
             this.label4.TabIndex = 44;
             this.label4.Text = "Reportes de Venta ";
             // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(18, 490);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(206, 37);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 55;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(19, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 18);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "Rango Personalizado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(20, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 18);
+            this.label2.TabIndex = 57;
+            this.label2.Text = "Busqueda por Factura";
+            // 
             // RVenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -332,6 +412,7 @@
             this.Text = "RVenta";
             this.Load += new System.EventHandler(this.RVenta_Load);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
@@ -339,6 +420,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.btnrestaurar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.minimizar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.salir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -362,5 +444,10 @@
         private System.Windows.Forms.PictureBox minimizar;
         private System.Windows.Forms.PictureBox salir;
         private System.Windows.Forms.PictureBox btnrestaurar;
+        private FontAwesome.Sharp.IconButton btnBuscar;
+        private System.Windows.Forms.TextBox textBoxFactura;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
