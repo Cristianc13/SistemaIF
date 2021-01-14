783c980d6e392edce2bafdbb01ba8112e50ed4ed
Alberto José Blandón Moreno <ialbertbla98@gmail.com>
2021-01-13 20:53:22 -0600

    cambios y correcciones

--- SistemaFacturacion/WIN/WINReportes/RVenta.Designer.cs
+++ SistemaFacturacion/WIN/WINReportes/RVenta.Designer.cs
@@ -152,7 +152,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(20, 530);
+            this.label3.Location = new System.Drawing.Point(20, 531);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(169, 18);
             this.label3.TabIndex = 58;
