ebd5c73cc144bcf61c83ed4d2d76ac3fbd168825
Alberto José Blandón Moreno <ialbertbla98@gmail.com>
2021-03-09 20:55:17 -0600

    Descuento

--- SistemaFacturacion/ENT/ENTDetalleVenta.cs
+++ SistemaFacturacion/ENT/ENTDetalleVenta.cs
@@ -7,9 +7,8 @@
         public string producto { get; set; }
         public decimal cantidadProducto { get; set; }
         public decimal precioSalida { get; set; }
-
+        public decimal descuento { get; set; }
         public decimal importe { get; set; }
-
         public int Fk_idCliente { get; set; }
     }
 }
